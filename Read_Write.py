7cd5f33113ee8e0ab9dc63673539fbe4c07880dd
Abhay Pratap Singh <74309121+AbhayPratap007@users.noreply.github.com>
2022-12-26 14:38:24 +0530

    Update Read_Write.py

--- Read_Write.py
+++ Read_Write.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Mar  1 15:51:09 2020
-
-@author: Abhinav
-"""
-
 #-------------------------Reading & Writing data in Files----------------------
 
 import pandas
